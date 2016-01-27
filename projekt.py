f13c84c76db66d4b1d56b4c9c45b560f88aa3dbe
Magda Szpor <magdaszpor@gmail.com>
2016-01-27 21:51:18 +0100

    Wczorawjsza wersja - odczytywanie wzoru z obrazka - wzorzec 1

--- projekt.py
+++ projekt.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jan 15 19:35:03 2016
+import numpy as np
+import matplotlib.pyplot as plt
 
-@author: aga magda kasia
-"""
-
+from skimage import data
+from skimage.feature import match_template
+#from imread import imread, imsave
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,7 +16,9 @@ from skimage.measure import label
 from skimage.morphology import closing, square
 from skimage.measure import regionprops
 from skimage.color import label2rgb, rgb2gray
+from skimage.data import imread
 
+#WCZYTYWANIE OBRAZKA
 image = imread('o.jpg')
 image = np.invert(image)
 image = rgb2gray(image)
@@ -51,3 +52,100 @@ for region in regionprops(label_image):
     ax.add_patch(rect)
 
 plt.show()
+
+
+#WCZYTYWANIE WZORU
+wzor = imread('wzor.jpg')
+wzor = np.invert(wzor)
+wzor = rgb2gray(wzor)
+
+#POROWNYWANIE WZORU Z OBRAZKIEM
+result = match_template(image, wzor)
+result2=result
+
+fig = plt.figure(figsize=(12, 5))
+ax1 = plt.subplot(1, 3, 1)
+ax2 = plt.subplot(1, 3, 2 )
+ax3 = plt.subplot(1, 3, 3, sharex=ax2, sharey=ax2)
+
+
+
+
+ax1.imshow(wzor)
+ax1.set_axis_off()
+ax1.set_title('template')
+
+
+
+#WYSZUKIWANIE NAJWIEKSZYCH WARTOSCI
+
+
+ax2.imshow(image)
+ax2.set_axis_off()
+ax2.set_title('image')
+hwzor, wwzor = wzor.shape
+
+ij = np.unravel_index(np.argmax(result2), result.shape)
+x, y = ij[::-1]
+
+rect = plt.Rectangle((x, y), wwzor, hwzor, edgecolor='r', facecolor='none')
+ax2.add_patch(rect)
+
+result2[ij]=0
+
+
+#kl[0]=np.unravel_index(np.argmax(result2), result.shape)
+#z, m= kl[::-1]
+
+
+i = 0
+while (np.argmax(result2) > 5000):
+    
+    ij=np.unravel_index(np.argmax(result2), result.shape)
+    x, y= ij[::-1]
+    result2[ij]=0
+    i=i+1
+    rect = plt.Rectangle((x, y), wwzor, hwzor, edgecolor='r', facecolor='none')
+    ax2.add_patch(rect)
+
+
+
+
+
+
+
+
+# highlight matched region
+
+#rect = plt.Rectangle((x[0], y[0]), wwzor, hwzor, edgecolor='r', facecolor='none')
+#rect2 = plt.Rectangle((z, m), wwzor, hwzor, edgecolor='r', facecolor='none')
+#ax2.add_patch(rect)
+#ax2.add_patch(rect2)
+
+
+
+
+
+
+ax3.imshow(result)
+ax3.set_axis_off()
+ax3.set_title('`match_template`\nresult')
+# highlight matched region
+ax3.autoscale(False)
+#ax3.plot(x, y, 'o', markeredgecolor='r', markerfacecolor='none', markersize=10)
+
+
+plt.show()
+
+#kanal red
+#image[0:39 , 0:46, 0]
+
+
+
+
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.imshow(result)
+
+plt.show()
+
+
